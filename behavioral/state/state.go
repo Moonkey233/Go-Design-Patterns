9d971d4538690b5d3f81a75b6b4eefe7add6911e
Moonkey233 <Moonkey233@foxmail.com>
2025-01-23 18:07:55 +0800

    策略模式

--- behavioral/state/state.go
+++ behavioral/state/state.go
@@ -1,3 +1,65 @@
 // 状态模式
 
 package state
+
+import "fmt"
+
+// state 不同状态需要实现的接口
+type state interface {
+	open(*door)
+	close(*door)
+}
+
+// door 门对象
+type door struct {
+	id           int
+	opened       state
+	closed       state
+	damaged      state
+	currentState state // 当前状态
+}
+
+func (d *door) open() {
+	d.currentState.open(d)
+}
+
+func (d *door) close() {
+	d.currentState.close(d)
+}
+
+func (d *door) setState(s state) {
+	d.currentState = s
+}
+
+// opened 开启状态
+type opened struct{}
+
+func (o *opened) open(d *door) {
+	fmt.Printf("%v门已开启\n", d.id)
+}
+
+func (o *opened) close(d *door) {
+	fmt.Printf("%v关闭成功\n", d.id)
+}
+
+// closed 关闭状态
+type closed struct{}
+
+func (c *closed) open(d *door) {
+	fmt.Printf("%v开启成功\n", d.id)
+}
+
+func (c *closed) close(d *door) {
+	fmt.Printf("%v门已关闭\n", d.id)
+}
+
+// damaged 损坏状态
+type damaged struct{}
+
+func (a *damaged) open(d *door) {
+	fmt.Printf("%v门已损坏，无法开启\n", d.id)
+}
+
+func (a *damaged) close(d *door) {
+	fmt.Printf("%v门已损坏，无法关闭\n", d.id)
+}
--- behavioral/state/state_test.go
+++ behavioral/state/state_test.go
@@ -1,1 +1,31 @@
 package state
+
+func ExampleState() {
+	d := &door{id: 1}
+
+	// 开启状态
+	op := &opened{}
+	d.setState(op)
+	d.open()
+	d.close()
+
+	// 关闭状态
+	cl := &closed{}
+	d.setState(cl)
+	d.open()
+	d.close()
+
+	// 损坏状态
+	dm := &damaged{}
+	d.setState(dm)
+	d.open()
+	d.close()
+
+	// Output:
+	// 1门已开启
+	// 1关闭成功
+	// 1开启成功
+	// 1门已关闭
+	// 1门已损坏，无法开启
+	// 1门已损坏，无法关闭
+}
